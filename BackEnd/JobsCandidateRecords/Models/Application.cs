b13673bb718841e0d6b4d784406d9f3162db7392
StanislavEmpty <129095409+StanislavEmpty@users.noreply.github.com>
2024-07-11 19:42:29 +0300

    Fix Application model empty by merges

--- BackEnd/JobsCandidateRecords/Models/Application.cs
+++ BackEnd/JobsCandidateRecords/Models/Application.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobsCandidateRecords.Models
+{
+    [Table("Application")]
+    public class Application
+    {
+        public int Id { get; set; }
+
+        public int CandidateId { get; set; }
+        public virtual Candidate? Candidate { get; set; }
+
+        public int? EmployeeWhoCreatedId { get; set; }
+        public virtual Employee? EmployeeWhoCreated { get; set; }
+
+        public DateTime? CreationDate { get; set; } = DateTime.Now;
+
+        [MaxLength(255)]
+        public string Details { get; set; } = string.Empty;
+
+        public virtual ICollection<ApplicaionsForRequests>? ApplicaionsForRequests { get; set; }
+
+        public virtual ICollection<ApplicationStatusHistory>? ApplicationStatusHistories { get; set; }
+        public virtual ICollection<Notes>? Notes { get; set; }
+        public virtual ICollection<Attachment>? Attachments { get; set; }
+    }
+}
